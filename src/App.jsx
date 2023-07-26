86f60ed5c496eeb2d915205cf748fb659bdd022b
mr <behzodbeki65@gmail.com>
2023-07-26 21:28:17 +0500

    fvqgervqerbtr5655

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthContex } from "./context/AuthContex";
-import UserLayout from "../src/components/Layout/UserLayout"
+import UserLayout from "../src/components/Layout/UserLayout@*"
 import AdminLayout from "./components/layout/AdminLayout";
 
 import HomeP from "./pages/User/HomeP";
